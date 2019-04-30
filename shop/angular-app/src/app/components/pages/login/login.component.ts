5f6063777abd912eb5d6c0e6918d533ef20b0af8
thales <thalfm@gmail.com>
2019-04-29 23:18:34 -0300

    iniciando com angular

---
+++ shop/angular-app/src/app/components/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+    credential = {
+        email: '',
+        password: ''
+    };
+
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit() {
+    }
+
+}
