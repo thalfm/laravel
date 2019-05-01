d5870de68bccc85392cb7bb85862fa60e774a9a2
thales <thalfm@gmail.com>
2019-05-01 00:30:45 -0300

    database blog

---
+++ blog/database/migrations/2019_04_20_152235_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->addColumn('integer', 'user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title', 40);
+            $table->string('description', 2000);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
